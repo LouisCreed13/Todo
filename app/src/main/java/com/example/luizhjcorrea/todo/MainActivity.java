83203cbbd62d6bea2e19b7643c3bb274f7525d66
luiz.hjcorrea <luiz.henrique18@yahoo.com>
2019-05-02 09:43:36 -0300

    Inserção de dados -  OK

--- app/src/main/java/com/example/luizhjcorrea/todo/MainActivity.java
+++ app/src/main/java/com/example/luizhjcorrea/todo/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.luizhjcorrea.todo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.luizhjcorrea.todo.Banco.ControlaBanco;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void insere (View view){
+        ControlaBanco crud = new ControlaBanco(getBaseContext());
+        EditText edNome = findViewById(R.id.txtNome);
+        TextView tvResultado = findViewById(R.id.tvResultado);
+
+        String nome = edNome.getText().toString();
+        String resultado = crud.insereDado(nome);
+        tvResultado.setText(resultado);
+
+    }
+
 }
